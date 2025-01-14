811329302bcddb05f50d19eb3b8ff50efdb31938
Jakob-SA <agergaardskovjakob@gmail.com>
2025-01-14 16:19:12 +0100

    Function added for updating question space

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,14 +10,15 @@ import org.jspace.*;
 public class Main {
 
     public final static String GATE_URI = "tcp://127.0.0.1:9001/?keep";
-    public final static String REMOTE_URI = "tcp://127.0.0.1:9001/Names?keep";
+    public final static String NAMES_URI = "tcp://127.0.0.1:9001/Names?keep";
+    public final static String QNA_URI = "tcp://127.0.0.1:9001/QnA?keep";
 
 
 
     public static void main(String[] argv) throws InterruptedException, IOException {
         SpaceRepository repository = new SpaceRepository();
         repository.addGate(GATE_URI);
-        String[] spaces = {"Names", "QNA", "GameState"};
+        String[] spaces = {"Names", "QnA", "GameState"};
 
         for (String space : spaces) {
             repository.add(space, new SequentialSpace());
@@ -29,26 +30,34 @@ public class Main {
 
         repository.put("GameState", GameState.ANSWERING);
 
-        Space remoteSpace = new RemoteSpace(REMOTE_URI);
-        Object[] temp = remoteSpace.queryp(new FormalField(String.class));
+        Space nameSpace = new RemoteSpace(NAMES_URI);
+        Object[] temp = nameSpace.queryp(new FormalField(String.class));
 
         repository.put("Names", "Lucas");
 
         for (Object row : temp) {
             System.out.println(row);
         }
+        Space QnASpace = new RemoteSpace(QNA_URI);
+        QnASpace.put("Word: TEST", "Meaning: TEST");
+        getNewQnA(QnASpace);
 
-        var api = API.getInstance();
-        Params params = new Params();
-        params.setLimit("5");
-        var response = api.callUrbanDictionaryAPI(params);
 
-        for (var wordDefinition : response) {
-            System.out.println(wordDefinition);
-        }
+
+
+
         repository.closeGates();
         System.exit(0);
     }
 
-
+    private static void getNewQnA(Space space) throws InterruptedException {
+        var api = API.getInstance();
+        Params params = new Params();
+        params.setLimit("3");
+        var response = api.callUrbanDictionaryAPI(params);
+        space.getAll(new FormalField(Object.class), new FormalField(Object.class));
+        for (var word : response) {
+            space.put(word.getWord(), word.getMeaning());
+        }
+    }
 }
